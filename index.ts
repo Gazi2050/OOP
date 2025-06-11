740b68c6f4d00f093dd6d669f0447fa14888f3b6
Gazi2050 <gazi45344@gmail.com>
2025-06-12 00:55:53 +0600

    add Person class

--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
 const person = {
     name: "Gazi",
     age: 23,
-    address: {
-        city: "Ctg",
-        country: "Bangladesh",
-    },
 };
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age
+    }
+
+    getPerson() {
+        return `name:${this.name},age:${this.age}`
+    }
+}
+
+const user = new Person('jack', 35);
+console.log(user);
