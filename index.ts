914dc5deea8ffd86cfafe72bbc4f208b184aef45
Gazi2050 <gazi45344@gmail.com>
2025-06-12 08:55:57 +0600

    update

--- index.ts
+++ index.ts
@@ -19,3 +19,51 @@ class Person implements person {
 
 const user = new Person('jack', 35);
 console.log(user);
+
+abstract class Account {
+    balance: number;
+
+    constructor(balance: number) {
+        this.balance = balance;
+    }
+
+    deposit(amount: number) {
+        this.balance += amount;
+    }
+
+    // Method without body – must be implemented in child class
+    abstract withdraw(amount: number): void;
+}
+
+class CurrentAccount extends Account {
+    withdraw(amount: number) {
+        const fee = 2;
+        const total = amount + fee;
+
+        if (this.balance >= total) {
+            this.balance -= total;
+        } else {
+            console.log("Not enough money");
+        }
+    }
+}
+
+class SavingsAccount extends Account {
+    withdraw(amount: number) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+        } else {
+            console.log("Not enough money");
+        }
+    }
+}
+
+const jack = new CurrentAccount(1000);
+jack.deposit(200);
+jack.withdraw(500);
+console.log(jack);
+
+const emma = new SavingsAccount(1500);
+emma.deposit(100);
+emma.withdraw(300);
+console.log(emma);
